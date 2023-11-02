590b9d7bde011d3132a3dd9bf828d610e046dec1
Wanderson-Patricio <118014817+Wanderson-Patricio@users.noreply.github.com>
2023-11-02 13:21:16 -0300

    Update drawer.py

--- drawer.py
+++ drawer.py
@@ -3,7 +3,6 @@ from particle import particle
 from pygame import display, draw
 
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
 
 def drawLine(line : line, screen : display) -> None:
     draw.line(screen, WHITE, (line.a.r.x, line.a.r.y), (line.b.r.x, line.b.r.y), line.size)
